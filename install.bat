7cb620a216d405ba0df63d54b6adb9fd97633e7a
d4NY0 <30463136+d4NY0H@users.noreply.github.com>
2021-10-26 22:29:02 +0200

    Add files via upload

--- install.bat
+++ install.bat
@@ -1,7 +1,6 @@
 @echo OFF
-:: --- v1.0.2021 by d4NY0 ---
 set TITLE=Quest-Sideloader
-TITLE %TITLE%
+title %TITLE%
 
 :: Set official platform tools download url.
 set PTURL="https://dl.google.com/android/repository/platform-tools-latest-windows.zip"
@@ -22,71 +21,82 @@ set PATH=%PATH%;%ADBDIR%\
 :: apk is required.
 if not exist *.apk goto Empty
 
+:: Start the adb server.
+echo [97mStarting the ADB server.[0m
+adb start-server
+
 :: Wait until device is connected.
-echo Waiting for the device.
+echo [97mWaiting for the device...[0m
 adb wait-for-device
 
 :: Get device informations.
-echo Querying device information.
 for /f "tokens=9" %%a in ('adb shell ip route') do (set ipaddrt=%%a)
 for /f "tokens=1,2" %%i in ('adb shell dumpsys battery') do ^if "%%i"=="level:" set LVL=%%j
 for /f "tokens=4" %%i in ('adb shell df -h /data/media') do set STG=%%i
 :: Update the title.
-TITLE %TITLE%-Connected-[Battery:%LVL%]-[IP:%ipaddrt%]-[Free space %STG%]
+title %TITLE%-Connected-[Battery:%LVL%]-[IP:%ipaddrt%]-[Free space %STG%]
 
 :: Show devices.
-adb devices
+adb devices -l
+echo(
 
 :: Check for install.txt
-@if exist install.txt goto txt
+if exist install.txt goto txt
 
 :: Install every apk in this folder.
 for /f "delims=|" %%i in ('dir /b "*.apk"') do (
-	echo Installing %%~ni.apk
+	echo [97mInstalling %%~ni.apk[0m
 	adb install -g -r "%%i"
+	if "%ERRORLEVEL%" NEQ "0" goto Error
+	echo(
 )
 
 :: Copy each folder to obb directory.
 for /d %%d in (*.*) do (
-	@echo Copy obb folder %%d to /sdcard/Android/obb/
+	echo [97mCopy obb folder %%d to /sdcard/Android/obb/[0m
 	adb push %%d /sdcard/Android/obb/%%d/
+	if "%ERRORLEVEL%" NEQ "0" goto Error
+	echo(
 )
-@if "%ERRORLEVEL%" NEQ "0" (goto Error) else (goto Success)
+goto Success
 
 :txt
 :: Execute each line inside install.txt.
-echo Execute install.txt:
-FOR /F "USEBACKQ TOKENS=*" %%A IN (install.txt) do (
+echo [97mExecute install.txt:[0m
+for /F "USEBACKQ TOKENS=*" %%A in (install.txt) do (
     cmd /c "%%~A"
+	if "%ERRORLEVEL%" NEQ "0" goto Error
 )
-@if "%ERRORLEVEL%" NEQ "0" (goto Error) else (goto Success)
+echo(
+goto Success
 
 :Success
-@echo [92mInstallation successful.[0m
-@pause
+echo [92mInstallation successful.[0m
+pause
 goto Quit
 
 :Error
-@echo [91mError installing the APK or the obb file.[0m
-@pause
+echo(
+echo [91mError installing the APK or the obb file.[0m
+pause
 goto Quit
 
 :Empty
-@echo [91mNo apk file found in this folder.[0m
-@pause
+echo [91mNo apk file found in this folder.[0m
+pause
 goto Quit
 
 :DirError
-@echo [91mError! The required ADB files could not be found:[0m
-@echo %ADBDIR%\adb.exe
-@echo %ADBDIR%\AdbWinApi.dll
-@echo %ADBDIR%\AdbWinUsbApi.dll
-@echo(
-@echo Download from %PTURL%
-@echo(
+echo [91mError! The required ADB files could not be found:[0m
+echo %ADBDIR%\adb.exe
+echo %ADBDIR%\AdbWinApi.dll
+echo %ADBDIR%\AdbWinUsbApi.dll
+echo(
+echo Download from %PTURL%
+echo(
 set /p INPUT=Do you want to download it now? [y/n] 
-IF /I '%INPUT%'=='y' GOTO Download
-IF /I '%INPUT%'=='n' GOTO Quit
+if /I '%INPUT%'=='y' goto Download
+if /I '%INPUT%'=='n' goto Quit
 
 :Download
 @echo(
@@ -96,9 +106,9 @@ curl --ssl-no-revoke -L "%PTURL%" -o "%DOCDIR%\platform-tools-latest-windows.zip
 powershell -Command "Expand-Archive -Force '%DOCDIR%\platform-tools-latest-windows.zip' -DestinationPath '%DOCDIR%'"
 :: Delete zip file.
 del /q %DOCDIR%\platform-tools-latest-windows.zip
-@echo(
-@echo [92mSucessfully downloaded the neccessary files.[0m
-@echo(
+echo(
+echo [92mSucessfully downloaded the neccessary files.[0m
+echo(
 timeout 1 >nul
 :: Reload this script.
 call "%~f0"
