77b36612478b7bf6602188a40fbdfa06f34e451c
d4NY0 <30463136+d4NY0H@users.noreply.github.com>
2021-10-27 10:34:59 +0200

    Add files via upload

--- install.bat
+++ install.bat
@@ -10,16 +10,16 @@ set DOCDIR=%USERPROFILE%\Documents
 set ADBDIR=%DOCDIR%\platform-tools
 
 :: Check for platform-tools dir and required adb files.
-if not exist %ADBDIR%\ goto DirError
-if not exist %ADBDIR%\adb.exe goto DirError
-if not exist %ADBDIR%\AdbWinApi.dll goto DirError
-if not exist %ADBDIR%\AdbWinUsbApi.dll goto DirError
+if not exist %ADBDIR%\ goto dirError
+if not exist %ADBDIR%\adb.exe goto dirError
+if not exist %ADBDIR%\AdbWinApi.dll goto dirError
+if not exist %ADBDIR%\AdbWinUsbApi.dll goto dirError
 
 :: Add adb dir to path variable.
 set PATH=%PATH%;%ADBDIR%\
 
 :: apk is required.
-if not exist *.apk goto Empty
+if not exist *.apk goto notFound
 
 :: Start the adb server.
 echo|set /p="[97mStarting the ADB server... [0m"
@@ -29,24 +29,19 @@ echo [92mDONE[0m
 :: Wait until device is connected.
 echo|set /p="[97mWaiting for the device... [0m"
 adb wait-for-device
-
-:: Get device informations.
-for /f "tokens=9" %%a in ('adb shell ip route') do (set ipaddrt=%%a)
-for /f "tokens=1,2" %%i in ('adb shell dumpsys battery') do ^if "%%i"=="level:" set LVL=%%j
-for /f "tokens=4" %%i in ('adb shell df -h /data/media') do set STG=%%i
-:: Update the title.
-title %TITLE%-Connected-[Battery: %LVL%]-[IP: %ipaddrt%]-[Free space: %STG%]
+:: Get device informations and update the title.
+call:updateTitle
 echo [92mDONE[0m
 echo(
 
 :: Check for install.txt
-if exist install.txt goto txt
+if exist install.txt goto txtInstall
 
 :: Install every apk in this folder.
 for /f "delims=|" %%i in ('dir /b "*.apk"') do (
 	echo [97mInstalling %%~ni.apk[0m
 	adb install -g -r "%%i"
-	if "%ERRORLEVEL%" NEQ "0" goto Error
+	if "%ERRORLEVEL%" NEQ "0" goto errorMsg
 	echo(
 )
 
@@ -54,38 +49,40 @@ for /f "delims=|" %%i in ('dir /b "*.apk"') do (
 for /d %%d in (*.*) do (
 	echo [97mCopy obb folder %%d to /sdcard/Android/obb/[0m
 	adb push %%d /sdcard/Android/obb/%%d/
-	if "%ERRORLEVEL%" NEQ "0" goto Error
+	if "%ERRORLEVEL%" NEQ "0" goto errorMsg
 	echo(
 )
-goto Success
+goto successMsg
 
-:txt
+:txtInstall
 :: Execute each line inside install.txt.
 echo [97mExecute install.txt:[0m
 for /f "USEBACKQ TOKENS=*" %%A in (install.txt) do (
 	cmd /c "%%~A"
-	if "%ERRORLEVEL%" NEQ "0" goto Error
+	if "%ERRORLEVEL%" NEQ "0" goto errorMsg
 )
 echo(
-goto Success
+goto successMsg
 
-:Success
+:successMsg
+:: Get device informations and update the title.
+call:updateTitle
 echo [92mInstallation successful.[0m
 pause
-goto Quit
+goto quitScript
 
-:Error
+:errorMsg
 echo(
 echo [91mError installing the APK or the obb file.[0m
 pause
-goto Quit
+goto quitScript
 
-:Empty
+:notFound
 echo [91mNo apk file found in this folder.[0m
 pause
-goto Quit
+goto quitScript
 
-:DirError
+:dirError
 echo [91mError! The required ADB files could not be found:[0m
 echo %ADBDIR%\adb.exe
 echo %ADBDIR%\AdbWinApi.dll
@@ -94,10 +91,10 @@ echo(
 echo Download from %PTURL%
 echo(
 set /p INPUT=Do you want to download it now? [y/n] 
-if /I '%INPUT%'=='y' goto Download
-if /I '%INPUT%'=='n' goto Quit
+if /I '%INPUT%'=='y' goto downloadAdb
+if /I '%INPUT%'=='n' goto quitScript
 
-:Download
+:downloadAdb
 @echo(
 :: Download platform tools with curl.
 curl --ssl-no-revoke -L "%PTURL%" -o "%DOCDIR%\platform-tools-latest-windows.zip"
@@ -111,8 +108,17 @@ echo(
 timeout 1 >nul
 :: Reload this script.
 call "%~f0"
-goto Quit
+goto quitScript
 
-:Quit
+:updateTitle
+:: Get device informations.
+for /f "tokens=9" %%a in ('adb shell ip route') do (set ipaddrt=%%a)
+for /f "tokens=1,2" %%i in ('adb shell dumpsys battery') do ^if "%%i"=="level:" set LVL=%%j
+for /f "tokens=4" %%i in ('adb shell df -h /data/media') do set STG=%%i
+:: Update the title.
+title %TITLE%-Connected-[Battery: %LVL%]-[IP: %ipaddrt%]-[Free space: %STG%]
+exit /b 0
+
+:quitScript
 adb kill-server
 exit
